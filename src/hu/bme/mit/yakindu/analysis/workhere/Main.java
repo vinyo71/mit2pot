720e0f04c4bfedf5363ae504a99762d8df55a455
Vass Viktor <eaviktor17@gmail.com>
2021-06-21 14:17:16 +0200

    4.6 done

--- src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -1,12 +1,20 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.stext.stext.EventDefinition;
+import org.yakindu.sct.model.stext.stext.VariableDefinition;
 
 import hu.bme.mit.model2gml.Model2GML;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
 
 public class Main {
@@ -27,9 +35,9 @@ public class Main {
 		
 		// Reading model
 		Statechart s = (Statechart) root;
-		TreeIterator<EObject> iterator = s.eAllContents();
-		while (iterator.hasNext()) {
-			EObject content = iterator.next();
+		TreeIterator<EObject> it = s.eAllContents();
+		while (it.hasNext()) {
+			EObject content = it.next();
 			if(content instanceof State) {
 				State state = (State) content;
 				if(statePrev != null)
@@ -38,10 +46,92 @@ public class Main {
 			}
 		}
 		
+		// 4.3
+		/*
+		it = s.eAllContents();
+		while(it.hasNext()) {
+			EObject obj = it.next();
+			if(obj instanceof VariableDefinition) {
+				VariableDefinition vardef = (VariableDefinition) obj;
+				System.out.println(vardef.getName());
+			}
+			if(obj instanceof EventDefinition) {
+				EventDefinition eventdef = (EventDefinition) obj;
+				System.out.println(eventdef.getName());
+			}		
+		}
+		*/
+		
+		// 4.4
+		/*
+		it = s.eAllContents();
+		System.out.println("public static void print(IExampleStatemachine s) {");
+		while(it.hasNext()) {
+			EObject obj = it.next();
+			if(obj instanceof VariableDefinition) {
+			VariableDefinition var = (VariableDefinition) obj;
+				String varname = var.getName();
+				String name = varname.substring(0, 1).toUpperCase().concat(varname.substring(1));
+				System.out.println("System.out.println(\""+varname+" = \" + s.getSCInterface().get"+name+"());");
+			}
+		}
+		System.out.println("}");
+		*/
+		
+		// 4.5
+		it = s.eAllContents();
+		System.out.println("");
+		System.out.println("public static void main(String[] args) throws IOException {");
+		System.out.println("ExampleStatemachine s = new ExampleStatemachine();");
+		System.out.println("s.setTimer(new TimerService());");
+		System.out.println("RuntimeService.getInstance().registerStatemachine(s, 200);");
+		System.out.println("s.init();");
+		System.out.println("s.enter();");
+		System.out.println("s.runCycle();");
+		System.out.println("");
+		System.out.println("Scanner in = new Scanner(System.in);");
+		System.out.println("String next;");
+		System.out.println("while (true) {");
+		System.out.println("next = in.nextLine();");
+		boolean first = true;
+		while(it.hasNext()) {
+			EObject obj = it.next();
+			if(obj instanceof EventDefinition) {
+				EventDefinition event = (EventDefinition) obj;
+				String eventname = event.getName();
+				String raisename = eventname.substring(0, 1).toUpperCase().concat(eventname.substring(1));
+				if (first)
+					System.out.println("if (next.equals(\""+eventname+"\")) {");
+				else
+					System.out.println("} else if (next.equals(\""+eventname+"\")) {");
+				System.out.println("s.raise"+raisename+"();");
+				System.out.println("s.runCycle();");
+				System.out.println("print(s);");
+			}
+		}
+		System.out.println("} else if (next.equals(\"exit\")) {");
+		System.out.println("System.exit(0);");
+		System.out.println("}");
+		System.out.println("}");
+		System.out.println("}");
+		it = s.eAllContents();
+		System.out.println("");
+		System.out.println("public static void print(IExampleStatemachine s) {");
+		while(it.hasNext()) {
+		EObject obj = it.next();
+		if(obj instanceof VariableDefinition) {
+			VariableDefinition var = (VariableDefinition) obj;
+			String varname = var.getName();
+			String name = varname.substring(0, 1).toUpperCase().concat(varname.substring(1));
+			System.out.println("System.out.println(\""+varname+" = \" + s.getSCInterface().get"+name+"());");
+		}
+		}
+		System.out.println("}");
+		
 		// check if statechart has trap state
-		hasTrap(s);
+		//hasTrap(s);
 		// check if statechart has nameless state and advise name for that
-		adviseName(s);
+		//adviseName(s);
 		
 		// Transforming the model into a graph representation
 		String content = model2gml.transform(root);
